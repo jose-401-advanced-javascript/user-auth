7f53edacb29bfdf4fdf265b239d32db82ae95abe
Jose <j.a.ojeda@live.com>
2019-10-08 22:41:23 -0700

    wrote and tested path to get list of users

--- e2e/__tests__/auth.test.js
+++ e2e/__tests__/auth.test.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 const { signupUser, signupAdmin } = require('../data-helpers');
 
 describe('Auth API', () => {
-
   beforeEach(() => dropCollection('users'));
 
   const testUser = {
@@ -15,8 +14,7 @@ describe('Auth API', () => {
   let user = null;
 
   beforeEach(() => {
-    return signupUser(testUser)
-      .then(newUser => user = newUser);
+    return signupUser(testUser).then(newUser => (user = newUser));
   });
 
   it('signs up a user', () => {
@@ -45,10 +43,18 @@ describe('Auth API', () => {
     });
   }
 
-  testEmailAndPasswordRequired('signup', 'email', { password: 'I no like emails' });
-  testEmailAndPasswordRequired('signup', 'password', { email: 'no@password.com' });
-  testEmailAndPasswordRequired('signin', 'email', { password: 'I no like emails' });
-  testEmailAndPasswordRequired('signin', 'password', { email: 'no@password.com' });
+  testEmailAndPasswordRequired('signup', 'email', {
+    password: 'I no like emails'
+  });
+  testEmailAndPasswordRequired('signup', 'password', {
+    email: 'no@password.com'
+  });
+  testEmailAndPasswordRequired('signin', 'email', {
+    password: 'I no like emails'
+  });
+  testEmailAndPasswordRequired('signin', 'password', {
+    email: 'no@password.com'
+  });
 
   it('signs in a user', () => {
     return request
@@ -72,7 +78,7 @@ describe('Auth API', () => {
     });
   }
 
-  testBadSignup('rejects bad password', { 
+  testBadSignup('rejects bad password', {
     email: testUser.email,
     password: 'bad password'
   });
@@ -95,15 +101,23 @@ describe('Auth API', () => {
       .set('Authorization', jwt.sign({ foo: 'bar' }, 'shhhhh'))
       .expect(401);
   });
-
 });
 
 describe('Admin routes', () => {
-
   beforeEach(() => dropCollection('users'));
 
   const testUser = {
     email: 'me@me.com',
+    password: 'abc'
+  };
+
+  const testUser2 = {
+    email: 'me2@me.com',
+    password: 'abc'
+  };
+
+  const testUser3 = {
+    email: 'me3@me.com',
     password: 'abc'
   };
 
@@ -117,8 +131,8 @@ describe('Admin routes', () => {
 
   beforeEach(() => {
     return Promise.all([
-      signupUser(testUser).then(newUser => user = newUser),
-      signupAdmin(adminUser).then(newAdmin => admin = newAdmin)
+      signupUser(testUser).then(newUser => (user = newUser)),
+      signupAdmin(adminUser).then(newAdmin => (admin = newAdmin))
     ]);
   });
 
@@ -137,7 +151,7 @@ describe('Admin routes', () => {
       .put(`/api/auth/users/${user._id}/roles/admin`)
       .set('Authorization', admin.token)
       .expect(200)
-      .then(({ body }) => {            
+      .then(({ body }) => {
         return request
           .delete(`/api/auth/users/${body._id}/roles/admin`)
           .set('Authorization', admin.token)
@@ -146,5 +160,31 @@ describe('Admin routes', () => {
             expect(body.roles[0]).toBeUndefined;
           });
       });
+  });
+
+  it('gets a list of users', () => {
+    return Promise.all([signupUser(testUser2), signupUser(testUser3)]).then(
+      () => {
+        return request
+          .get('/api/auth/users')
+          .set('Authorization', admin.token)
+          .expect(200)
+          .then(({ body }) => {
+            expect(body.length).toBe(4);
+            expect(body[0]).toMatchInlineSnapshot(
+              {
+                _id: expect.any(String)
+              },
+              `
+              Object {
+                "_id": Any<String>,
+                "email": "me@me.com",
+                "roles": Array [],
+              }
+            `
+            );
+          });
+      }
+    );
   });
 });
