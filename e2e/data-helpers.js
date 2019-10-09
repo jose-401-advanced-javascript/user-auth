a8f8e6343a61178f756101b32b9f2313ede1691e
Jose <j.a.ojeda@live.com>
2019-10-08 22:19:48 -0700

    refactored data-helpers to signin admin

--- e2e/data-helpers.js
+++ e2e/data-helpers.js
@@ -1,8 +1,13 @@
 const request = require('./request');
-
+const User = require('../lib/models/user');
 
 const testUser = {
   email: 'me@me.com',
+  password: 'abc'
+};
+
+const adminUser = {
+  email: 'admin@me.com',
   password: 'abc'
 };
 
@@ -14,6 +19,24 @@ function signupUser(user = testUser) {
     .then(({ body }) => body);
 }
 
+function signupAdmin(user = adminUser) {
+  return signupUser(user)
+    .then(signupUser => {
+      return User.updateById(signupUser._id, { 
+        $addToSet: { roles: 'admin' }
+      });
+    })
+    .then(() => {
+      return request
+        .post('/api/auth/signin')
+        .send(user)
+        .expect(200)
+        .then(({ body }) => body);
+    });
+}
+
+
 module.exports = {
-  signupUser
+  signupUser,
+  signupAdmin
 };
